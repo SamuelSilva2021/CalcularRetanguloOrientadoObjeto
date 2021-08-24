388d382f7cfb7a9b25aa955624a0f4e2f8f81109
Samuca <79856359+SamuelSilva2021@users.noreply.github.com>
2021-08-23 21:43:04 -0300

    Adicionar arquivos de projeto.

---
+++ CalcularRetanguloOrientadoObjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalcularRetanguloOrientadoObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+            Console.WriteLine("Digite os valores do retangulo!");
+            Console.WriteLine("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ CalcularRetanguloOrientadoObjeto/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CalcularRetanguloOrientadoObjeto
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Altura + Largura) * 2;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+        
+    }
+}
